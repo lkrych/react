8adbdea9bf0f4e8fc8c48a2de824015827d13097
Leland Krych <Leland.Krych@gmail.com>
2017-07-08 12:22:08 -0700

    Import material UI static files into project

--- indio_challenge/src/App.js
+++ indio_challenge/src/App.js
@@ -1,19 +1,28 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './assets/react-toolbox/theme.css';
+import theme from './assets/react-toolbox/theme.js';
+import ThemeProvider from 'react-toolbox/lib/ThemeProvider';
+
+import Button from 'react-toolbox/lib/button/Button';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Indio React Challenge</h2>
+      <ThemeProvider theme={theme}>
+        <div className="App">
+          <div className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h2>Indio React Challenge</h2>
+          </div>
+          <p className="App-intro">
+            To get started, press this button
+            <br></br>
+            <Button label="Test Button" raised primary />
+          </p>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      </ThemeProvider>
     );
   }
 }
