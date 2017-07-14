3339d6c5240ffa252ae3b066aef80093c1cc1a45
Leland Krych <Leland.Krych@gmail.com>
2017-07-14 14:06:21 -0700

    display correct questiontype dynamically

--- indio_challenge/src/util/form-creator-helper.js
+++ indio_challenge/src/util/form-creator-helper.js
@@ -3,6 +3,27 @@ import NumberInput from '../components/form-input/number';
 import TextInput from '../components/form-input/text';
 import RadioButtons from '../components/form-input/radio';
 
+const makeComponent = (questionType, item, key, idx) => {
+  let componentHash = {
+    "text": <div key = {idx}>
+              <p>{item[key].questionText}</p>
+              <TextInput
+                subInputs = {item[key].subInputs} />
+            </div>,
+    "number": <div key = {idx}>
+                <p>{item[key].questionText}</p>
+                <NumberInput
+                  subInputs = {item[key].subInputs} />
+              </div> ,
+    "radio": <div key = {idx}>
+                <p>{item[key].questionText}</p>
+                <RadioButtons
+                  subInputs = {item[key].subInputs} />
+              </div>
+  };
+
+  return componentHash[questionType];
+};
 
 export const displayConditional = (questionType, value, subInputs) => {
   const subQuestions = [];
@@ -10,38 +31,21 @@ export const displayConditional = (questionType, value, subInputs) => {
     subInputs.forEach((item,idx) => {
       let key = Object.keys(item)[0];
       if (item[key]['conditionText'] === value){
-        let jsx = <div key = {idx}>
-                    <p>{item[key].questionText}</p>
-                    <RadioButtons
-                      subInputs = {item[key].subInputs} />
-                  </div>;
-        subQuestions.push(jsx);
+        subQuestions.push(makeComponent(item[key].questionType, item, key, idx));
       }
     });
   } else if (questionType == "number") {
     subInputs.forEach((item,idx) => {
       let key = Object.keys(item)[0];
       if (item[key]['conditionText'] === value){
-        subQuestions.push(
-          <div key = {idx}>
-            <p>{item[key].questionText}</p>
-            <NumberInput
-              subInputs = {item[key].subInputs} />
-          </div>
-        );
+        subQuestions.push(makeComponent(item[key].questionType, item, key, idx));
       }
     });
   } else{
     subInputs.forEach((item,idx) => {
       let key = Object.keys(item)[0];
       if (item[key]['conditionText'] === value){
-        subQuestions.push(
-          <div key = {idx}>
-            <p>{item[key].questionText}</p>
-            <TextInput
-              subInputs = {item[key].subInputs} />
-          </div>
-        );
+        subQuestions.push(makeComponent(item[key].questionType, item, key, idx));
       }
     });
   }
